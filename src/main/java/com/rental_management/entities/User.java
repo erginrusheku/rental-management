95457bfd76c1dd4bcfddde62c4e4bf6ccf6eaca3
Ergin Rusheku <rushekuergin@gmail.com>
2024-06-19 14:00:20 +0200

    SuccessDTOs and ErrorDTO

--- src/main/java/com/rental_management/entities/User.java
+++ src/main/java/com/rental_management/entities/User.java
@@ -4,11 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
 import java.util.List;
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
