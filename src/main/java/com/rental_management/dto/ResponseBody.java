e7afaab20518f02822ec99896651e4a46a7d7f7f
igliq <igli@datawizsoft.com>
2024-06-19 12:39:59 +0200

    List Properties

--- src/main/java/com/rental_management/dto/ResponseBody.java
+++ src/main/java/com/rental_management/dto/ResponseBody.java
@@ -1,5 +1,11 @@
 package com.rental_management.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
 public class ResponseBody {
 
     private List<SuccessDTO> success;
