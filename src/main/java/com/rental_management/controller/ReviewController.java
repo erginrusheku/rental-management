38b703f70fac8211082db492f5bb0ed538156214
Erion <etobli20@epoka.edu.al>
2024-07-03 17:42:33 +0200

    Merge branch 'feature-develop-BE' of https://github.com/erginrusheku/rental-management into feature-develop-BE

--- src/main/java/com/rental_management/controller/ReviewController.java
+++ src/main/java/com/rental_management/controller/ReviewController.java
@@ -32,25 +32,20 @@ public class ReviewController {
     }
 
     @GetMapping("/reviewId/{reviewId}")
-    ResponseEntity<ReviewDTO> getById(@PathVariable Long reviewId){
+    ResponseEntity<ReviewDTO> getById(@RequestParam Long reviewId){
         ReviewDTO reviewIds = reviewService.getById(reviewId);
         return new ResponseEntity<>(reviewIds, HttpStatus.OK);
     }
 
-    @PutMapping({"/updateReview"})
+    @PutMapping("/updateReview")
     ResponseEntity<ResponseBody> updateReview(@RequestParam Long userId,@RequestParam Long propertyId,@RequestParam Long reviewId, @RequestBody List<ReviewDTO> reviewList){
         ResponseBody reviewAndId = reviewService.updateReviewByUserForProperty(userId, propertyId, reviewId, reviewList);
         return new ResponseEntity<>(reviewAndId, HttpStatus.OK);
     }
 
-    @DeleteMapping("/deleteReviewId/{reviewId}")
-    ResponseEntity<Void> deleteById(@PathVariable Long reviewId){
-        reviewService.deleteReview(reviewId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 
-    @PostMapping("/createReview/{userId}/{propertyId}")
-    ResponseEntity<ResponseBody> createReviewByUserForProperty(@PathVariable Long userId, @PathVariable Long propertyId, @RequestBody List<ReviewDTO> reviewList){
+    @PostMapping("/createReview")
+    ResponseEntity<ResponseBody> createReviewByUserForProperty(@RequestParam Long userId, @RequestParam Long propertyId, @RequestBody List<ReviewDTO> reviewList){
         ResponseBody createReview = reviewService.createReviewByUserForProperty(userId,propertyId,reviewList);
         return new ResponseEntity<>(createReview, HttpStatus.CREATED);
     }
